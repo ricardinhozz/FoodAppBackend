2f473d4d4ebbc790a9d6e63366587c6bbadc5ef0
Gabriel <95100329+ricardinhozz@users.noreply.github.com>
2023-02-13 17:22:45 -0300

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -1,1 +1,1 @@
-mongodb_uri = 'mongodb+srv://ricardinhozz:<Ss@24842953>@cluster0.5anb5uj.mongodb.net/?retryWrites=true&w=majority'
+mongodb_uri = 'mongodb+srv://censored:<censored>@cluster0.5anb5uj.mongodb.net/?retryWrites=true&w=majority'
